8f0815e22d343bc694bb3406b6f0ceef08e8107f
Stewart Rogers <rsrogers@ymail.com>
2025-05-03 09:27:48 -0700

    added try block around IP check and changed API call code

--- vpn_active.py
+++ vpn_active.py
@@ -2,13 +2,20 @@
 
 import urllib.request
 import sys
+import requests
 
 def main_ipcheck(xhomeip1):
-    xhomeip2 = urllib.request.urlopen('https://api.ipify.org/').read().decode('utf8')
-    if xhomeip1.strip() == xhomeip2.strip():
-        return 0
-    else:
-        return 1
+    try:
+      xhomeipR = requests.get("https://api.ipify.org?format=json", timeout=5)
+      xhomeip2 = xhomeipR.json().get("ip")
+#      xhomeip2 = urllib.request.urlopen('https://api.ipify.org/').read().decode('utf8')
+      if xhomeip1.strip() == xhomeip2.strip():
+         return 0
+      else:
+         return 1
+    except requests.RequestException:
+         return 0
+
 
 if __name__ == "__main__":
     xresult = main_ipcheck(sys.argv[1])
